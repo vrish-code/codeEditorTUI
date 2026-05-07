49b2a2687b6d7c333ee05054c5c6b845cdd21aca
Vrishan Somalinga <vrishansomalinga@gmail.com>
2026-05-07 15:23:59 +0530

    delete colors and pyc

--- pythonScripts/utils/editor.py
+++ pythonScripts/utils/editor.py
@@ -2,77 +2,83 @@ import os
 import time
 
 def commandModeEditor():
-    commandsGlobal=["editor --open", "editor --new", "editor --quit"]
+    commandsGlobal=["editor --open", "editor --new", "editor --quit", "read"]
     def commandMode(commands):
         print("___________________________________")
         print("##### COMMANDS #####")
         print(commands)
         print("_______________________")
     commandMode(commandsGlobal)
-    commEntered=input("Enter a command")
+    commEntered=input("Enter a command\n").strip()
     if commEntered in commandsGlobal:
      if commEntered=="editor --open":
-        filePath=input("Enter the file path to be opened.")
+        filePath=input("Enter the file path to be opened.\n").upper().strip()
         print("_______________________________")
-        if filePath.startswith("c:" "C:"):
+        if filePath.startswith("c:") or filePath.startswith("C:"):
             if os.path.exists(filePath):
                 with open(filePath, "r",encoding='utf-8') as f:
                     codeOpened=f.read()
                 print(codeOpened)
-                codeEdit=input("Edit code (press cl to stop editing)? (Y/n)").upper
-                print("_______________________________")()
+                print("_______________________________")
+
+                codeEdit=input("Edit code (press cl to stop editing)? (Y/n)").upper().strip()
                 if codeEdit=="Y":
                     newCode=[]
                     while True:
-                        codeLn=input(codeEdit)
+                        codeLn=input()
                         if codeLn=="cl":
                             break
                         newCode.append(codeLn)
                     if newCode:
-                        saveConf=input("Save code (Y/n)").upper()
+                        saveConf=input("Save code (Y/n)").upper().strip()
                         if saveConf=="Y":
                             progBar=""
                             for i in range(20):
                                 progBar+="#"
                                 print(f"[{progBar}]", end="\r", flush=True)
                                 time.sleep(0.1)
+                            print("\n")
                             print("__________________________________")
                             with open(filePath,"w", encoding='utf-8') as f:
                                 f.write("\n".join(newCode))
-                            commandModeGoBack=input("Go back to command mode? (Y/n)").upper()
+                            commandModeGoBack=input("Go back to command mode? (Y/n)").upper().strip()
                             print("_______________________________")
                             if commandModeGoBack=="Y":
-                                commandMode()
+                                commandMode(commandsGlobal)
                             else:quit()
                         else:
                             pass
                 else:
                     quit()
      elif commEntered=="editor --new":
-         fileNew=input("Enter the new file's path")
+         fileNew=input("Enter the new file's path\n").strip()
          print("_____________________________")
-         if fileNew.startswith("c:" "C:"):
+         if fileNew.startswith("c:") or fileNew.startswith("C:"):
              if not os.path.exists(fileNew):
                 newCode=[]
                 while True:
-                  codeLnNew=input("Enter the code you want to save to the file (press cl to stop editing)")
+                  codeLnNew=input("Enter the code you want to save to the file (press cl to stop editing)\n")
                   print("_______________________________")
                   if codeLnNew=="cl":
                     break
                   newCode.append(codeLnNew)
-                newCode.remove("cl")
+                progBar=""
                 for i in range(20):
                     progBar+="#"
                     print(f"[{progBar}]", end="\r", flush=True)
                     time.sleep(0.1)
-                    with open(fileNew,"w", encoding='utf-8') as f:
-                     f.write("\n".join(newCode))
+                print("\n")
+                with open(fileNew,"w", encoding='utf-8') as f:
+                    f.write("\n".join(newCode))
              else:
                  commChoice=("File path already exists. Go back to command mode to restart operation. (Y/n)").upper()
                  if commChoice=="Y":
-                     commandMode()
+                     commandMode(commandsGlobal)
                  else:
                      quit()
+         else:
+          print("### Invalid Path ###")
+
 commandModeEditor()
                  
 
